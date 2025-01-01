c43038429f60996892deb96e412ec7d349815dea
unpacify <114315772+unpacify@users.noreply.github.com>
2025-01-01 15:50:45 -0500

    Delete seleniumwire2/undetected_chromedriver/__init__.py

--- seleniumwire2/undetected_chromedriver/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py for seleniumwire2.undetected_chromedriver
-
-from .webdriver import Chrome, ChromeOptions  # Import Chrome and ChromeOptions
-try:
-    import undetected_chromedriver as uc  # Import the undetected_chromedriver module
-except ImportError:
-    uc = None  # Handle cases where undetected_chromedriver is not installed
