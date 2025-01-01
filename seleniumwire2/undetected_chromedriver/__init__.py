85c1fa0a3c15853220bd3443c524d6d9d211817a
unpacify <114315772+unpacify@users.noreply.github.com>
2025-01-01 13:53:30 -0500

    Update __init__.py

--- seleniumwire2/undetected_chromedriver/__init__.py
+++ seleniumwire2/undetected_chromedriver/__init__.py
@@ -1,5 +1,7 @@
-# seleniumwire2/undetected_chromedriver/__init__.py
+# __init__.py for seleniumwire2.undetected_chromedriver
 
-from .webdriver import Chrome, ChromeOptions
-
-
+from .webdriver import Chrome, ChromeOptions  # Import Chrome and ChromeOptions
+try:
+    import undetected_chromedriver as uc  # Import the undetected_chromedriver module
+except ImportError:
+    uc = None  # Handle cases where undetected_chromedriver is not installed
